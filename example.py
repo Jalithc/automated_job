69725afdd2f5d9e0cd22dbd5a2764c424aa0d214
Jalithc <126810658+Jalithc@users.noreply.github.com>
2024-04-21 14:44:04 +0530

    Update example.py
    added comments

--- example.py
+++ example.py
@@ -1,9 +1,12 @@
+'''import libraries'''
 import schedule
 import time
 
+'''print the working progress'''
 def job():
     print("I'm working...")
 
+'''time sheduling'''
 schedule.every(10).minutes.do(job)
 schedule.every().hour.do(job)
 schedule.every().day.at("15:05").do(job)
@@ -11,6 +14,7 @@ schedule.every().monday.do(job)
 schedule.every().wednesday.at("13:15").do(job)
 schedule.every().minute.at(":17").do(job)
 
+'''run the sheduled items'''
 while True:
     schedule.run_pending()
     time.sleep(1)
